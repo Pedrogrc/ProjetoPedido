3a7a2687bfbb5b8bd7be1a71aa7d1e3297abcd12
Pedro Calixto <pedroca.calixto@gmail.com>
2019-03-11 22:11:27 -0300

    Criação dos Repositories, Classe Produto e Mudanças no Main

--- src/main/java/br/senai/sc/ProjetoPedidoApplication.java
+++ src/main/java/br/senai/sc/ProjetoPedidoApplication.java
@@ -1,13 +1,43 @@
 package br.senai.sc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.senai.sc.domain.Categoria;
+import br.senai.sc.domain.Produto;
+import br.senai.sc.repositories.CategoriaRepository;
+import br.senai.sc.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class ProjetoPedidoApplication {
+public class ProjetoPedidoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPedidoApplication.class, args);
 	}
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	@Autowired
+	private ProdutoRepository repro;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Produto p1 = new Produto(null,"Computador", 2000.00);
+		repo.save(cat1);
+		repro.save(p1);
+		
+		Categoria cat2 = new Categoria(null,"Escritório");
+		Produto p2 = new Produto(null,"Impressora", 800.00);
+		repo.save(cat2);
+		repro.save(p2);
+	}
+	
+	
+	
+	
 
 }
--- src/main/java/br/senai/sc/domain/Produto.java
+++ src/main/java/br/senai/sc/domain/Produto.java
@@ -1,47 +1,45 @@
 package br.senai.sc.domain;
 
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+public class Produto {
 
-public class Categoria {
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String nome;
 	
+	private Double preco;
 	
-	//Construtores
-	public Categoria(){
+	
+	public Produto(Integer id, String nome, Double preco) {
 		
-	}
-
-	public Categoria(Integer id, String nome) {
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
-	
-	//Getters e Setters
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 	
 	
